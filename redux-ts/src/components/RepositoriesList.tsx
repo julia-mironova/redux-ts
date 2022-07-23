d26e8fa622a06188fccd823d562d935f6f73affe
Yuliya Mironova <rabkevich15@gmail.com>
2022-07-23 19:37:30 +0200

    feat: provider error

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { actionCreators } from "../state";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
 	const [term, setTerm] = useState("");
-	const dispatch = useDispatch();
+	const { searchRepos } = useActions();
+	const { data, error, loading } = useTypedSelector(
+		(state) => state.repositories
+	);
 
 	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		dispatch(actionCreators.searchRepos(term));
+		searchRepos(term);
+		//console.log("i am onsubmit ");
 	};
 
 	return (
@@ -17,6 +21,9 @@ const RepositoriesList: React.FC = () => {
 				<input value={term} onChange={(e) => setTerm(e.target.value)} />
 				<button>Search</button>
 			</form>
+			{error && <h4>{error}</h4>}
+			{loading && <h4>loading...</h4>}
+			{!error && !loading && data}
 		</div>
 	);
 };
